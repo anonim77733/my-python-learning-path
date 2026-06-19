40312695a212397b522658d3c6170383cce576b5
Anonim77733 <anonimunknown211@gmail.com>
2026-06-19 18:24:50 +0200

    bool,operators etc.

---
+++ lesson.py
@@ -0,0 +1,40 @@
+# x=90
+# y=90
+# if x>y:
+#     print(x,"is greater than",y)
+# elif x==y:
+#     print(x,"is equal with",y)
+# else:
+#     print(x,"is less than",y)        
+
+# x=bool(0)  #false 
+# y=bool("") #false
+# z=23       #true
+# t="python" #true
+# print(x)
+# print(y)
+
+# print(bool([]))   #false
+# print(bool({}))   #false
+# print(bool(()))   #false
+# print(bool(None)) #false
+
+# def myFunction():
+#     return True          #true oldugu ucun yes
+
+# if myFunction():
+#     print("Yes")
+# else:
+#     print("No")
+
+# x=70
+# print(isinstance(x,int))  #true
+
+#number1=12
+#number2=3
+# result=number1%number2  #qaligi gorsedir 0 ,meselen 12 7 olsa qalig 5 olar
+# print(result)
+
+num1=19
+num2=7
+print(num1//num2)
