bcbfb75f7c8aab36422f8034e2ce669a29c7e6dd
Anonim77733 <anonimunknown211@gmail.com>
2026-06-23 10:59:35 +0200

    lesson py

--- lesson.py
+++ lesson.py
@@ -1,40 +1,84 @@
-# x=90
-# y=90
+
+# x=5
+# x &=7
+# print(x) #5 bu and-di defterde qeydlerde,+sekillerde izahi var
+
+# x=5
+# x |=7
+# print(x)  #7 bu or-du
+
+# x=5
+# x ^=7
+# print(x)  #2  bu xor-du 
+
+# x=16
+# x >>=2
+# print(x)  #4 buda rightshift defterde qeydlere bax, >> verilen say qeder saga surusdurur riyazda x bol 2 usdu n
+
+# x=5
+# x <<=2
+# print(x)  #20 buda sola surusdurur riyaziyyatda x vurulsun 2 usdu n
+
+# x=5
+# print(x:=7) #7 buda sadece verdiyimiz ededi atir x-a yeni 5 idi 7 eledik
+
+
+# x=7
+# y=15
+# if x!=y:
+#     print(f"{x} is not equal {y}")
+# else:
+#     print(f"{x} equal with {y}")    
+
+
+# x=19
+# y=18
 # if x>y:
-#     print(x,"is greater than",y)
+#     print(f"{x} is greater than {y}") 
+
 # elif x==y:
-#     print(x,"is equal with",y)
+#     print(f"{x} is equal {y}")
+
 # else:
-#     print(x,"is less than",y)        
+#     print(f"{x} is less than {y}")    
 
-# x=bool(0)  #false 
-# y=bool("") #false
-# z=23       #true
-# t="python" #true
-# print(x)
-# print(y)
 
-# print(bool([]))   #false
-# print(bool({}))   #false
-# print(bool(()))   #false
-# print(bool(None)) #false
+#tek ve cut eded tapma praqrami
+# number=int(input("Enter a number:"))
 
-# def myFunction():
-#     return True          #true oldugu ucun yes
-
-# if myFunction():
-#     print("Yes")
+# if number<0:
+#     print("The entered number cannot be less than zero.")
+# elif number%2 ==0:
+#     print(f"{number} is an even number.") #cut
 # else:
-#     print("No")
+#     print(f"{number} is an odd number")   #tek     
 
-# x=70
-# print(isinstance(x,int))  #true
+#EXAM SCORE
+# user_score=int(input("Your Exam Score: "))
+# if user_score<=30:
+#     print("You failed the exam.")
+# elif user_score>30 and user_score<=85:
+#     print("You passed the exam.")
+# else:
+#     print("Yo're amazing!")
 
-#number1=12
-#number2=3
-# result=number1%number2  #qaligi gorsedir 0 ,meselen 12 7 olsa qalig 5 olar
-# print(result)
 
-num1=19
-num2=7
-print(num1//num2)
+# x=7
+# print(not(x>3 and x<10))  #false cunki verdiyimiz sert dogru olsada qarsina not dedik deye trunu false edir 
+
+# y=4
+# print(not(y>1 and y<2))  #true, burdada icerisi false idi amma nota gore true oldu
+
+# x=["apple","strawberry","orange"]
+# y=["apple","strawberry","orange"]
+# z=x
+
+# print(x is z) #true cunki x-i z-ye atdig
+# print(x is y) #false cunki iclerindekiler eyni olsada ferqli deyisenlerin icindediler
+# print(x==y)  #true cunki icindekiler eynidir
+# print(x is not z)  #false
+# print(x is not y)  #true
+# print(x!=y)       #false
+
+x=~3
+print(x)
